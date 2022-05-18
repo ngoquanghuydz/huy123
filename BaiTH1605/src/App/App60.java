778fa4f801ad3fb65e053a07ea55770df3f95acf
ngoquanghuydz <97440991+ngoquanghuydz@users.noreply.github.com>
2022-05-18 21:42:54 +0700

    Ngo Quang Huy 2021050875

---
+++ BaiTH1605/src/App/App60.java
@@ -0,0 +1,28 @@
+
+package App;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class App60 {
+
+    public static void main(String[] args) {
+        Set<String> LinkedHashSet = new LinkedHashSet<String>();
+        LinkedHashSet.add("Java");
+        LinkedHashSet.add("C++");
+        LinkedHashSet.add("C#");
+        LinkedHashSet.add("C");
+        LinkedHashSet.add("php");
+
+        System.out.println("Các phần tử trong LinkedHashSet: ");
+        System.out.println(LinkedHashSet);
+
+        String nameDelete = "C#";
+        if (LinkedHashSet.contains(nameDelete)) {
+            LinkedHashSet.remove(nameDelete);
+
+            System.out.println("Các phần tử trong LinkedHashSet: ");
+            System.out.println(LinkedHashSet);
+
+        }
+    }
+}
---
+++ BaiTH1605/src/App/App98.java
@@ -0,0 +1,14 @@
+
+package App;
+import java.util.TreeMap;
+import java.util.Map;
+import java.util.HashMap;
+
+public class App98 {
+    public static void main(String[] args) {
+        TreeMap<String, Double> treeMap = new TreeMap<>();
+        Map<Float, Double> map = new HashMap<>();
+        TreeMap<Float, Integer> treeMap1 =new TreeMap<>();
+    }
+    
+}
